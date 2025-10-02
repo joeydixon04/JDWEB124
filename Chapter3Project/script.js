2defc71f0b1ca7dc0fb6c13095457eeaaece5e43
joeydixon04 <tjdixonusa@gmail.com>
2025-10-01 22:42:43 -0500

    Add files via upload

---
+++ Chapter3Project/script.js
@@ -0,0 +1,28 @@
+/* Joey Dixon 10/1/25*/
+
+
+let fullName = "Joey Dixon";
+console.log("Full Name:", fullName);
+
+
+let desiredSalary = 100000;
+console.log("Desired Salary:", desiredSalary);
+
+
+let isVeteran = false; 
+console.log("Veteran Status:", isVeteran);
+
+
+let friends = ["Haiden", "Nick", "Mike"];
+console.log("Friends:", friends);
+
+
+let friendsSalaries = [100000, 200000, 75000];
+console.log("Friends' Salaries:", friendsSalaries);
+
+let anotherFriend = {
+  firstName: "Taylor",
+  lastName: "King",
+  desiredSalary: 50000
+};
+console.log("Another Friend:", anotherFriend);
